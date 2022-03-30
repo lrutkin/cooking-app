1d3683ab320cd728df88ccdd394039ab44249e5a
Leslie Rutkin <lrutkin@gmail.com>
2022-03-29 20:05:36 -0700

    create index and js to convert gallons to liters

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function multiply(number1, number2) {
+  return number1 * number2;
+  }
+  
+  const number1 = parseFloat(prompt("Enter the number of gallons:"));
+
+  const number2 = 3.785;
+  
+  alert(multiply(number1, number2) + " liters");
